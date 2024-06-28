6cb4f56aa5ae8955b2c16375f0cafe0dffd71ad0
ayushcoder11 <ayushpatil.patil10@gmail.com>
2024-06-28 16:38:59 +0530

    Added about us page and fixed sidebar login signup button

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -49,18 +49,18 @@ function HomeLayout({children}){
                 
                 <div className="drawer-content">
 
+                    <label htmlFor="my-drawer" className="cursor-pointer relative">
+                        <FiMenu size={"32px"} className='font-bold text-white m-4' onClick={changeWidth}/>
+                    </label>
+
                 </div>
 
-                <label htmlFor="my-drawer" className="cursor-pointer relative">
-                    <FiMenu size={"32px"} className='font-bold text-white m-4' onClick={changeWidth}/>
-                </label>
-
-                <div className='drawer-side w-0'>
+                <div className='drawer-side w-0 '>
 
                     <label htmlFor="my-drawer" className="drawer-overlay">
                     </label>
 
-                    <ul className='menu p-4 w-48 sm:w-80 bg-base-300 text-base-content relative'>
+                    <ul className='menu p-4 w-48 h-[100%] sm:w-80 bg-base-100 text-base-content relative'>
 
                         <li className='w-fit absolute right-2 z-50'>
 
@@ -71,7 +71,7 @@ function HomeLayout({children}){
                         </li>
                         
                         <li>
-                            <Link to='/Home'>Home
+                            <Link to='/'>Home
                             </Link>
                         </li>
 
@@ -96,9 +96,9 @@ function HomeLayout({children}){
                         </li>
 
                         {!isLoggedIn && (
-                            <li>
+                            <li className="absolute bottom-4 w-[90%]">
                                 <div className='w-full flex items-center justify-center gap-1'>
-                                    <button className=' bg-[#3B71CA] px-4 py-2   font-semibold rounded-md w-1/2 hover:bg-[#5A90E5] transition-all ease-in-out duration-300'>
+                                    <button className=' bg-[#3B71CA] px-4 py-2 font-semibold rounded-md w-1/2 hover:bg-[#5A90E5] transition-all ease-in-out duration-300'>
                                         <Link to='/login' className='text-black'>
                                             Login
                                         </Link>
@@ -113,7 +113,7 @@ function HomeLayout({children}){
                         )}
 
                         {isLoggedIn && (
-                            <li>
+                            <li className="absolute bottom-4 w-[90%]">
                                 <div className='w-full flex items-center justify-center gap-1'>
                                     <button className=' bg-[#3B71CA] px-4 py-2   font-semibold rounded-md w-1/2 hover:bg-[#5A90E5] transition-all ease-in-out duration-300'>
                                         <Link to='/user/profile' className='text-black'>
